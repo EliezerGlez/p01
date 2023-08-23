995dea915e0f670f90cf34e29123401405ba6819
EliezerGlez <ELIEZERGLEZ@GMAIL.COM>
2023-08-23 18:51:37 +0100

    asfsadf

---
+++ src/Paquete01/Clase01.java
@@ -0,0 +1,11 @@
+package Paquete01;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Proyecto01 - Paquete01 - Clase01");
+
+	}
+
+}
