d819391abdab64dd24c271634efa5b9a72663cb9
EliezerGlez <ELIEZERGLEZ@GMAIL.COM>
2023-08-23 19:01:31 +0100

    pasdfasldfasdf

---
+++ src/Paquete01/Clase03.java
@@ -0,0 +1,10 @@
+package Paquete01;
+
+public class Clase03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Paquete02/Clase01.java
@@ -0,0 +1,10 @@
+package Paquete02;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
